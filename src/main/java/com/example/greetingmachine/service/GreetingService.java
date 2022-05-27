21af8ca03610d06d3591d78058b793535e5f76f2
gulumser.aslan <gulumser.aslan@trendyol.com>
2022-05-27 11:01:34 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/java/com/example/greetingmachine/service/GreetingServiceTest.java

--- src/main/java/com/example/greetingmachine/service/GreetingService.java
+++ src/main/java/com/example/greetingmachine/service/GreetingService.java
@@ -1,5 +1,6 @@
 package com.example.greetingmachine.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -10,6 +11,11 @@ public class GreetingService {
         if (ObjectUtils.isEmpty(name)) {
             return "Hello, World";
         }
-        return "Hello, " + name;
+        if (StringUtils.contains(name,",")) {
+            String replacedName = StringUtils.replace(name, ",", " and");
+            return "Hello, " + replacedName;
+
+        }
+        return StringUtils.isAllUpperCase(name) ? "HELLO, " + name : "Hello, " + name;
     }
 }
--- src/test/java/com/example/greetingmachine/service/GreetingServiceTest.java
+++ src/test/java/com/example/greetingmachine/service/GreetingServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.greetingmachine.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,5 +24,28 @@ class GreetingServiceTest {
 
         //then
         assertThat(greet).isEqualTo("Hello, Bora");
+    }
+
+    @Test
+    void it_should_greet_person_without_name() {
+        //given
+
+        //when
+        String greet = greetingService.greet(StringUtils.EMPTY);
+
+        //then
+        assertThat(greet).isEqualTo("Hello, World");
+    }
+
+    @Test
+    void it_should_greet_couple() {
+        //given
+        String name = "Annie, Bart";
+
+        //when
+        String greet = greetingService.greet(name);
+
+        //then
+        assertThat(greet).isEqualTo("Hello, Annie and Bart");
     }
 }
